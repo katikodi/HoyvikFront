1cc27f3a9250a8b35796e58f557ab670fe262be6
elias-kodehode <elias96.kodehode@gmail.com>
2026-07-02 10:51:09 +0200

    test

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export default function Footer() {
     return (
-        <div className="footer" style={{border: "1px solid white"}}>
+        <div className="footer" style={{border: "1px solid gray"}}>
             <h1>Footer</h1>
             {/* <div>
                 <Link to="/">
