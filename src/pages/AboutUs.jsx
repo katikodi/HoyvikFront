6fe0b927f9fc47fa79f0cc6ab1219f2bee1cbc73
Mathias <4mathias.kodehode@gmail.com>
2026-07-29 14:04:17 +0200

    add button and rename css classes/ids

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -3,10 +3,19 @@ import "../styles/AboutUs.css";
 export default function AboutUs() {
     return (
         <>
-            <div className="container">
+            <div
+                id="mainContainer"
+                className="columnContent"
+            >
                 <h1>{title}</h1>
-                <div className="contentContainer">
-                    <p>{para}</p>
+                <div className="rowContent centerContent">
+                    <div
+                        id="contentContainer"
+                        className="columnContent"
+                    >
+                        <p>{para}</p>
+                        <button>BUTTON BUTTON BUTTON</button>
+                    </div>
                     <img src="https://cdn.discordapp.com/attachments/1508798814847373344/1519992055034675220/image.png?ex=6a6a6b2a&is=6a6919aa&hm=ffec9725f186b34b59581e5f6e256ddeece9eaefe96c32697feedde06d148f27&" />
                 </div>
             </div>
