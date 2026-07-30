586ede11d8051ce2dd435fcd3f394f2fc908da73
jakob <aesthaltics@pm.me>
2026-07-30 14:27:19 +0200

    added icon section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,45 @@
 import Booking from "../components/Booking";
+import "../styles/Home.css";
+
+// TODO: is there a better way of importing these urls?
 import heroImageUrl from "../images/heroImage.jpg";
 import calendarIconUrl from "../icons/calendarIcon.svg";
-import "../styles/Home.css";
+import turstierIconUrl from "@/icons/manHiking.svg";
+import cabinUrl from "@/icons/cabinWithSmokingChimney.svg";
+import vikingUrl from "@/icons/vikingHelmet.svg";
+import boatUrl from "@/icons/boatOnTrailer.svg";
 
 // TODO: move all this styling into css files
 // TODO: change colors to use the css variables
+// TODO: fix the fonts on this page
 
 export default function Home() {
+    const icons = [
+        {
+            id: crypto.randomUUID(),
+            iconURL: cabinUrl,
+            text: "Hytter & Leiligheter"
+        },
+        {
+            id: crypto.randomUUID(),
+            iconURL: vikingUrl,
+            text: "Vikingaktiviteter"
+        },
+        {
+            id: crypto.randomUUID(),
+            iconURL: boatUrl,
+            text: "Båtutleie"
+        },
+        {
+            id: crypto.randomUUID(),
+            iconURL: turstierIconUrl,
+            text: "Turstier"
+        }
+    ];
     return (
         <div style={{ display: "flex", flexDirection: "column" }}>
             <Hero />
+            <IconSection icons={icons} />
         </div>
     );
 }
@@ -20,10 +50,12 @@ const Hero = () => {
             style={{
                 backgroundImage: `linear-gradient(93deg, rgba(62, 85, 70, 0.47) 1.68%, rgba(96, 141, 111, 0.08) 52.57%, rgba(125, 188, 146, 0.00) 97.9%), linear-gradient(106deg, rgba(0, 0, 0, 0.26) 13.4%, rgba(255, 255, 255, 0.00) 100%), linear-gradient(180deg, #1A1A1B 0%, rgba(75, 75, 78, 0.15) 49.73%, rgba(124, 124, 129, 0.00) 100%), url(${heroImageUrl}`,
                 backgroundBlendMode: "normal, darken, normal",
-                height: "50rem",
+                height: "100dvh",
                 backgroundRepeat: "no-repeat",
                 backgroundSize: "100% auto",
-                backgroundPosition: "center"
+                backgroundPosition: "center",
+                display: "flex",
+                flexDirection: "column"
             }}
         >
             <Nav />
@@ -190,8 +222,17 @@ const ShadowGradient = () => {
 };
 
 const HeroBooking = () => {
+    const buttonHeight = 4;
     return (
-        <div style={{ paddingInline: "18rem" }}>
+        <div
+            style={{
+                paddingLeft: "12rem",
+                paddingRight: "6rem",
+                marginTop: "auto",
+                position: "relative",
+                transform: "translateY(30%)"
+            }}
+        >
             <div
                 style={{
                     background: "var(--Background-2, #B8CBBE)",
@@ -199,7 +240,7 @@ const HeroBooking = () => {
                     display: "flex",
                     flexDirection: "row",
                     paddingInline: "1.8rem",
-                    paddingBlock: "3.2rem ",
+                    paddingBlock: "1rem ",
                     gap: "1rem",
                     justifyContent: "space-around",
                     width: "100%",
@@ -211,43 +252,48 @@ const HeroBooking = () => {
                     labelText={"Innsjekk"}
                     icon={calendarIconUrl}
                     inputId={crypto.randomUUID()}
+                    height={buttonHeight}
                 />
                 <HeroBookingInput
                     inputType={"date"}
                     labelText={"Innsjekk"}
                     icon={calendarIconUrl}
                     inputId={crypto.randomUUID()}
+                    height={buttonHeight}
                 />
                 <HeroBookingInput
                     inputType={"date"}
                     labelText={"Innsjekk"}
                     icon={calendarIconUrl}
                     inputId={crypto.randomUUID()}
+                    height={buttonHeight}
                 />
                 <HeroBookingInput
                     inputType={"date"}
                     labelText={"Innsjekk"}
                     icon={calendarIconUrl}
                     inputId={crypto.randomUUID()}
+                    height={buttonHeight}
                 />
                 <Button
                     text={"SJEKK TILGJENGELIGHET"}
                     onClick={() => console.log("cluck")}
+                    height={buttonHeight}
                 ></Button>
             </div>
         </div>
     );
 };
 
-const HeroBookingInput = ({ inputType, labelText, icon, inputId }) => {
+const HeroBookingInput = ({ inputType, labelText, icon, inputId, height }) => {
     return (
-        <div style={{ display: "flex", flexDirection: "column", flexGrow: "1" }}>
+        <div style={{ display: "flex", flexDirection: "column", flexGrow: "1", gap: "0.3rem" }}>
             <label
                 htmlFor={inputId}
                 style={{
                     color: "var(--Text-text1, #271C22)",
                     fontFamily: "Montserrat",
-                    fontSize: "1.25rem",
+                    fontSize: "0.75rem",
                     fontStyle: "normal",
                     fontWeight: "400",
                     lineHeight: "normal"
@@ -263,7 +309,7 @@ const HeroBookingInput = ({ inputType, labelText, icon, inputId }) => {
                     border: "2px solid var(--Struktur, #2A3430)",
                     display: "flex",
                     width: "100%",
-                    height: "2.5125rem",
+                    height: `${height}rem`,
                     alignItems: "center",
                     fontSize: "2.25rem"
                 }}
@@ -272,12 +318,12 @@ const HeroBookingInput = ({ inputType, labelText, icon, inputId }) => {
     );
 };
 
-const Button = ({ text, onClick }) => {
+const Button = ({ text, onClick, height }) => {
     return (
         <button
             style={{
                 display: "flex",
-                height: "2.5125rem",
+                height: `${height}rem`,
                 padding: "0 1.0625rem",
                 justifyContent: "center",
                 alignItems: "center",
@@ -287,5 +333,57 @@ const Button = ({ text, onClick }) => {
         >
             {text}
         </button>
+    );
+};
+
+const IconSection = ({ icons }) => {
+    return (
+        <div
+            style={{
+                display: "inline-flex",
+                // height: "10.8125rem",
+                padding: "5.1875rem 3.8125rem 5.25rem 3.8125rem",
+                justifyContent: "center",
+                alignItems: "center",
+                background: "var(--Background, #678A73)",
+                boxShadow: "0 4px 4px 0 rgba(0, 0, 0, 0.25)",
+                display: "flex",
+                flexDirection: "row",
+                paddingInline: "3.2rem",
+                gap: "14rem"
+            }}
+        >
+            {icons.map(icon => (
+                <IconSectionIcon
+                    key={icon.id}
+                    icon={icon.iconURL}
+                    text={icon.text}
+                />
+            ))}
+        </div>
+    );
+};
+
+const IconSectionIcon = ({ icon, text }) => {
+    return (
+        <div
+            style={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center"
+            }}
+        >
+            <img
+                style={{ width: "5rem", height: "5rem", aspectRatio: "1/1" }}
+                src={icon}
+                alt={`icon av ${text}`}
+            />
+            <p
+                style={{ alignSelf: "stretch", textWrap: "nowrap", textAlign: "center" }}
+                className="thicc-monsterrat"
+            >
+                {text}
+            </p>
+        </div>
     );
 };
