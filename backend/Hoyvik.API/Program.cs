61dd81a6d3b7e872576c43d7a95a7ab2e1d94fe5
elias-kodehode <elias96.kodehode@gmail.com>
2026-09-01 13:30:39 +0200

    refactored and cleaned up program.cs

--- backend/Hoyvik.API/Program.cs
+++ backend/Hoyvik.API/Program.cs
@@ -1,77 +1,14 @@
-using FluentValidation;
+using Hoyvik.API;
 using Hoyvik.API.Data;
 using Hoyvik.API.Endpoints;
-using Hoyvik.API.Services;
-using Hoyvik.API.Validators;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.FileProviders;
-using Stripe;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.AddServiceDefaults();
-builder.Services.AddProblemDetails();
-
-builder.Services.AddCors(options => options.AddPolicy("frontend",
-    p => p.WithOrigins("http://localhost:54131")
-        .AllowAnyHeader()
-        .AllowAnyMethod()
-        .AllowCredentials()));
-
-builder.Services.AddAuthorizationBuilder()
-    .AddDefaultPolicy("Guest", p => p.RequireRole("guest"))
-    .AddPolicy("User", p => p.RequireRole("user"))
-    .AddPolicy("Admin", p => p.RequireRole("admin"));
+builder.AddApplication();
 
 
-builder.Services.AddScoped<ImageUploaderService>();
-builder.Services.AddScoped<IBookingService, BookingService>();
-builder.Services.AddValidatorsFromAssemblyContaining<CreateSessionValidator>();
-builder.Services.AddHostedService<BookingExpirationService>();
 
-StripeConfiguration.ApiKey = builder.Configuration["Stripe:SecretKey"] ?? throw new Exception("Stripe:SecretKey is missing");
-
-
-builder.Services
-    .AddIdentity<ApplicationUser, IdentityRole>(x =>
-    {
-        x.Password.RequireDigit = false;
-        x.Password.RequireUppercase = false;
-        //x.Password.RequiredLength = 0;
-        x.Password.RequireLowercase = false;
-        x.Password.RequireNonAlphanumeric = false;
-    })
-    .AddDefaultTokenProviders()
-    .AddEntityFrameworkStores<Database>();
-
-
-builder.Services.RegisterEndpoints();
-
-builder.AddNpgsqlDbContext<Database>("database");
-
-
-builder.Services.ConfigureApplicationCookie(x =>
-{
-    x.Cookie.HttpOnly = true;
-    x.Cookie.SameSite = SameSiteMode.Lax;
-    x.Cookie.SecurePolicy = builder.Environment.IsDevelopment()
-        ? CookieSecurePolicy.None
-        : CookieSecurePolicy.Always;
-
-
-    x.Events.OnRedirectToLogin = ctx =>
-    {
-        ctx.Response.StatusCode = StatusCodes.Status401Unauthorized;
-        return Task.CompletedTask;
-    };
-
-    x.Events.OnRedirectToAccessDenied = ctx =>
-    {
-        ctx.Response.StatusCode = StatusCodes.Status403Forbidden;
-        return Task.CompletedTask;
-    };
-
-});
 
 
 
@@ -96,7 +33,9 @@ if (app.Environment.IsProduction())
 
 app.UseAuthentication();
 app.UseAuthorization();
+
 app.UseStatusCodePages();
+
 app.MapApiEndpoints();
 
 
