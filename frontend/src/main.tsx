51ca8905e1f5d00302f9922514b67f80f295f875
elias-kodehode <elias96.kodehode@gmail.com>
2026-09-03 14:59:36 +0200

    can start re-creating the website

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,14 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-
 import "./index.css";
-import App from "./App.tsx";
+import App from "./App.jsx";
 import { ThemeProvider } from "@/components/theme-provider.tsx";
+import { AuthProvider } from "./auth/AuthProvider.jsx";
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
         <ThemeProvider>
-            <App />
+            <AuthProvider>
+                <App />
+            </AuthProvider>
         </ThemeProvider>
     </StrictMode>
 );
