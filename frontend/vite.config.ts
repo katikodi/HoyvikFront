3eb54ea2b9fa42d5d15cb2bf8f6ffd22a8153113
elias-kodehode <elias96.kodehode@gmail.com>
2026-09-03 14:24:21 +0200

    hooked up frontend to backedn

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,14 +1,53 @@
-import path from "path";
 import tailwindcss from "@tailwindcss/vite";
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import babel from "@rolldown/plugin-babel";
+import { fileURLToPath, URL } from "node:url";
+import { defineConfig, loadEnv } from "vite";
 
-// https://vite.dev/config/
-export default defineConfig({
-    plugins: [react(), tailwindcss()],
-    resolve: {
-        alias: {
-            "@": path.resolve(import.meta.dirname, "./src")
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), "");
+    return {
+        plugins: [react(), tailwindcss(), babel({ presets: [reactCompilerPreset()] })],
+        server: {
+            port: 54131,
+            proxy: {
+                // Proxy API calls to the app service
+                "/api": {
+                    target: env.services__backend__https__0 || process.env.services__backend__http__0,
+                    changeOrigin: false,
+                    secure: false
+                },
+                "/content": {
+                    target: env.services__backend__https__0 || process.env.services__backend__http__0,
+                    changeOrigin: false,
+                    secure: false
+                }
+            }
+        },
+        resolve: {
+            alias: {
+                "@": fileURLToPath(new URL("./src", import.meta.url))
+            }
+        },
+        test: {
+            globals: true,
+            environment: "jsdom",
+            setupFiles: "./src/unitTests/setupTests.js"
         }
-    }
+    };
 });
+
+// import path from "path";
+// import tailwindcss from "@tailwindcss/vite";
+// import react from "@vitejs/plugin-react";
+// import { defineConfig } from "vite";
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//     plugins: [react(), tailwindcss()],
+//     resolve: {
+//         alias: {
+//             "@": path.resolve(import.meta.dirname, "./src")
+//         }
+//     }
+// });
