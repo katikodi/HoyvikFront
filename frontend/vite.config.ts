d05f5c69bed90df089282387876a69492567d339
Mathias <4mathias.kodehode@gmail.com>
2026-09-04 13:50:46 +0200

    remove obsolete vite config comments

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -45,18 +45,3 @@ export default defineConfig(({ mode }) => {
         }
     };
 });
-
-// import path from "path";
-// import tailwindcss from "@tailwindcss/vite";
-// import react from "@vitejs/plugin-react";
-// import { defineConfig } from "vite";
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//     plugins: [react(), tailwindcss()],
-//     resolve: {
-//         alias: {
-//             "@": path.resolve(import.meta.dirname, "./src")
-//         }
-//     }
-// });
