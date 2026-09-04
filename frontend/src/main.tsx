8d0e4c141247473bbe4eb23a1c8e381e48242524
aesthaltics <aesthaltics@pm.me>
2026-09-04 13:10:46 +0200

    initial tanstack router setup

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,16 +1,25 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom/client";
+import { RouterProvider, createRouter } from "@tanstack/react-router";
+import { routeTree } from "./routeTree.gen";
 import "./index.css";
-import App from "./App.jsx";
-import { ThemeProvider } from "@/components/theme-provider.tsx";
-import { AuthProvider } from "./auth/AuthProvider.jsx";
 
-createRoot(document.getElementById("root")!).render(
-    <StrictMode>
-        <ThemeProvider>
-            <AuthProvider>
-                <App />
-            </AuthProvider>
-        </ThemeProvider>
-    </StrictMode>
-);
+// Set up a Router instance
+const router = createRouter({
+    routeTree,
+    defaultPreload: "intent",
+    scrollRestoration: true
+});
+
+// Register things for typesafety
+declare module "@tanstack/react-router" {
+    interface Register {
+        router: typeof router;
+    }
+}
+
+const rootElement = document.getElementById("app")!;
+
+if (!rootElement.innerHTML) {
+    const root = ReactDOM.createRoot(rootElement);
+    root.render(<RouterProvider router={router} />);
+}
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,11 +3,20 @@ import react, { reactCompilerPreset } from "@vitejs/plugin-react";
 import babel from "@rolldown/plugin-babel";
 import { fileURLToPath, URL } from "node:url";
 import { defineConfig, loadEnv } from "vite";
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, process.cwd(), "");
     return {
-        plugins: [react(), tailwindcss(), babel({ presets: [reactCompilerPreset()] })],
+        plugins: [
+            tanstackRouter({
+                target: "react",
+                autoCodeSplitting: true
+            }),
+            react(),
+            tailwindcss(),
+            babel({ presets: [reactCompilerPreset()] })
+        ],
         server: {
             port: 54131,
             proxy: {
