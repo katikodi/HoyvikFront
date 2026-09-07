0c6155c6e154ac71c915102a89dfc21b9e822a45
elias-kodehode <elias96.kodehode@gmail.com>
2026-09-07 14:40:17 +0200

    added BookingCalendar

--- frontend/src/pages/BookingPage.jsx
+++ frontend/src/pages/BookingPage.jsx
@@ -1,31 +1,5 @@
-import { Calendar } from "@/components/ui/calendar";
-import { api } from "@/services/client";
-import { useEffect, useState } from "react";
+import { BookingCalendar } from "@/components/BookingCalendar";
 
 export function BookingPage() {
-    const [date, setDate] = useState(new Date());
-
-    useEffect(() => {
-        getOccupiedDates();
-        console.log(date);
-    }, [date]);
-
-    async function getOccupiedDates() {
-        await api.get("/bookings/occupied");
-        const result = await api("/bookings/occupied", {
-            method: "GET"
-        });
-        console.log(result);
-
-        const bookedDates = Array.from(data);
-    }
-
-    return (
-        <Calendar
-            mode="single"
-            selected={date}
-            onSelect={setDate}
-            className="rounded-lg border"
-        />
-    );
+    return <BookingCalendar onDateChange={e => console.log(e)} />;
 }
--- frontend/src/routes/_index/booking.jsx
+++ frontend/src/routes/_index/booking.jsx
@@ -1,3 +1,4 @@
+import { BookingPage } from "@/pages/BookingPage";
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/_index/booking")({
