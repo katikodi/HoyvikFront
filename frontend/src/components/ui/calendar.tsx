caf760c642fc12cc85e0247dce8bdc9371fa75d8
elias-kodehode <elias96.kodehode@gmail.com>
2026-09-07 14:46:01 +0200

    outlet lol

--- frontend/src/components/ui/calendar.tsx
+++ frontend/src/components/ui/calendar.tsx
@@ -2,8 +2,9 @@ import * as React from "react";
 import { cn } from "cn";
 import { DayPicker, getDefaultClassNames, type DayButton, type Locale } from "react-day-picker";
 
-import { Button, buttonVariants } from "@/components/ui/button";
+import { Button } from "@/components/ui/button";
 import { ChevronLeftIcon, ChevronRightIcon, ChevronDownIcon } from "lucide-react";
+import buttonVariants from "./buttonVariants";
 
 function Calendar({
     className,
