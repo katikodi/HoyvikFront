3f3baabf4f0d3b5fc55125ca835dd0ca295d513d
Mathias <4mathias.kodehode@gmail.com>
2026-09-07 13:38:43 +0200

    extract main layout component from route definition

---
+++ frontend/src/layouts/MainLayout.tsx
@@ -0,0 +1,53 @@
+import { Button } from "@/components/ui/button";
+import { useAuth } from "@/hooks/authContext";
+import { Link, Outlet, useNavigate } from "@tanstack/react-router";
+
+export default function MainLayout() {
+    const { user, logout } = useAuth();
+    const navigate = useNavigate();
+    async function handleLogout() {
+        await logout();
+        navigate({ to: "/" });
+    }
+
+    return (
+        <div>
+            <header>
+                <nav className="flex gap-4 p-4">
+                    <Link
+                        to="/"
+                        className="[&.active]:font-bold"
+                    >
+                        Home
+                    </Link>
+
+                    <Link
+                        to="/about"
+                        className="[&.active]:font-bold"
+                    >
+                        About
+                    </Link>
+
+                    <Link
+                        to="/profile"
+                        className="[&.active]:font-bold"
+                    >
+                        Profile
+                    </Link>
+                    {user ? (
+                        <Button onClick={handleLogout}>Logout</Button>
+                    ) : (
+                        <>
+                            <Link to="/login">Login</Link>
+                            <Link to="/register">Register</Link>
+                        </>
+                    )}
+                </nav>
+            </header>
+
+            <main>
+                <Outlet />
+            </main>
+        </div>
+    );
+}
