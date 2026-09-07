843024fe1c8c6b6b9ed6ab3dc29922d1a03caf6c
Mathias <4mathias.kodehode@gmail.com>
2026-09-07 10:16:39 +0200

    remove unused imports

--- frontend/src/routes/_main/index.jsx
+++ frontend/src/routes/_main/index.jsx
@@ -1,5 +1,5 @@
 import { useAuth } from "@/hooks/authContext";
-import { createFileRoute, Link, Outlet } from "@tanstack/react-router";
+import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/_main/")({
     component: MainLayout
