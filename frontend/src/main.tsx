239189a2253071b333c3ea02a828ab5cc5c73d12
elias-kodehode <elias96.kodehode@gmail.com>
2026-09-09 11:40:09 +0200

    .

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,9 @@
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import { AuthProvider } from "./auth/AuthProvider";
-import App from "./app";
-import { ThemeProvider } from "./components/theme-provider";
-import { queryClient } from "./lib/queryClient";
+import "@/index.css";
+import { AuthProvider } from "@/auth/AuthProvider";
+import App from "@/app";
+import { ThemeProvider } from "@/components/theme-provider";
+import { queryClient } from "@/lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 
 const rootElement = document.getElementById("app")!;
