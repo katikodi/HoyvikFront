e8d6a82b333b8a1b17f342504c37724451eb61c1
Elias <elias96.kodehode@gmail.com>
2026-09-13 21:15:13 +0200

    moved hamburger menu to root layout

--- frontend/src/layouts/MainLayout.tsx
+++ frontend/src/layouts/MainLayout.tsx
@@ -1,17 +1,4 @@
-import { Button } from "@/components/ui/button";
-import {
-    DropdownMenu,
-    DropdownMenuContent,
-    DropdownMenuGroup,
-    DropdownMenuItem,
-    DropdownMenuLabel,
-    DropdownMenuSeparator,
-    DropdownMenuTrigger
-} from "@/components/ui/dropdown-menu";
-import { Menu } from "lucide-react";
 import { Link, Outlet, useLocation } from "@tanstack/react-router";
-import { useTheme } from "@/components/theme-provider";
-import { useAuth } from "@/hooks/authContext";
 
 export default function MainLayout() {
     const location = useLocation();
@@ -40,7 +27,6 @@ export default function MainLayout() {
                             {text}
                         </Link>
                     ))}
-                    <HamburgerDropdown />
                 </nav>
             </header>
 
@@ -48,69 +34,5 @@ export default function MainLayout() {
                 <Outlet />
             </main>
         </div>
-    );
-}
-
-function HamburgerDropdown() {
-    const { setTheme } = useTheme();
-    const { user, logout } = useAuth();
-
-    return (
-        <DropdownMenu modal={false}>
-            <DropdownMenuTrigger asChild>
-                <Button
-                    variant="outline"
-                    size="icon"
-                    className="fixed right-4 top-4 z-50"
-                >
-                    <Menu />
-                    <span className="sr-only">Open menu</span>
-                </Button>
-            </DropdownMenuTrigger>
-
-            <DropdownMenuContent>
-                <DropdownMenuGroup>
-                    <DropdownMenuLabel>My Account</DropdownMenuLabel>
-
-                    <DropdownMenuItem asChild>
-                        <Link to="/profile">Profile</Link>
-                    </DropdownMenuItem>
-                    {user ? (
-                        <>
-                            <DropdownMenuItem asChild>
-                                <Link to="/profile">My Bookings</Link>
-                            </DropdownMenuItem>
-                            <DropdownMenuItem
-                                onClick={() => logout()}
-                                className="cursor-pointer"
-                            >
-                                Logout
-                            </DropdownMenuItem>
-                        </>
-                    ) : (
-                        <>
-                            <DropdownMenuItem
-                                onClick={() => logout()}
-                                className="cursor-pointer"
-                            >
-                                <Link to="/login">Login</Link>
-                            </DropdownMenuItem>
-                        </>
-                    )}
-                </DropdownMenuGroup>
-
-                <DropdownMenuSeparator />
-
-                <DropdownMenuGroup>
-                    <DropdownMenuLabel>Theme</DropdownMenuLabel>
-
-                    <DropdownMenuItem onClick={() => setTheme("light")}>Light</DropdownMenuItem>
-
-                    <DropdownMenuItem onClick={() => setTheme("dark")}>Dark</DropdownMenuItem>
-
-                    <DropdownMenuItem onClick={() => setTheme("system")}>System</DropdownMenuItem>
-                </DropdownMenuGroup>
-            </DropdownMenuContent>
-        </DropdownMenu>
     );
 }
