888898ff393b570a6584e80965217720357269b0
Elias <elias96.kodehode@gmail.com>
2026-09-13 16:17:40 +0200

    made it not a modal

--- frontend/src/components/mode-toggle.tsx
+++ frontend/src/components/mode-toggle.tsx
@@ -8,7 +8,7 @@ export function ModeToggle() {
     const { setTheme } = useTheme();
 
     return (
-        <DropdownMenu>
+        <DropdownMenu modal={false}>
             <DropdownMenuTrigger asChild>
                 <Button
                     variant="outline"
