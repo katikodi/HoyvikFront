f88f90e54edddfd4e4895f1bb9f8c813a3ca2fd5
Elias <elias96.kodehode@gmail.com>
2026-09-13 17:00:18 +0200

    made mainLayout add top padding based on the current page

--- frontend/src/layouts/MainLayout.tsx
+++ frontend/src/layouts/MainLayout.tsx
@@ -1,34 +1,22 @@
 import { ModeToggle } from "@/components/mode-toggle";
-import { Link, Outlet } from "@tanstack/react-router";
+import { Link, Outlet, useLocation } from "@tanstack/react-router";
 
 export default function MainLayout() {
+    const location = useLocation();
+
     const navLinks = [
-        {
-            to: "/",
-            text: "Home"
-        },
-        {
-            to: "/activities",
-            text: "Activities"
-        },
-        {
-            to: "/about",
-            text: "About"
-        },
-        {
-            to: "/contact",
-            text: "Contact Us"
-        },
-        {
-            to: "/shop",
-            text: "Shop"
-        }
+        { to: "/", text: "Home" },
+        { to: "/activities", text: "Activities" },
+        { to: "/about", text: "About" },
+        { to: "/contact", text: "Contact Us" },
+        { to: "/shop", text: "Shop" }
     ];
 
-    //the layouts should control the layout and structure of the page, not its children
+    const isHome = location.pathname === "/";
+
     return (
         <div className="flex min-h-dvh w-full flex-col">
-            {/* header does not belong inside of main */}
+            {/* Header does not belong inside of main */}
             <header className="absolute inset-x-0 top-0 z-50">
                 <nav className="flex w-full items-center justify-around gap-4 p-4">
                     {navLinks.map(({ to, text }) => (
@@ -40,11 +28,12 @@ export default function MainLayout() {
                             {text}
                         </Link>
                     ))}
+
                     <ModeToggle />
                 </nav>
             </header>
 
-            <main className="min-w-0 w-full flex-1">
+            <main className={`min-w-0 w-full flex-1 ${isHome ? "" : "pt-20"}`}>
                 <Outlet />
             </main>
         </div>
