10d3d42c097fd8e99b95a1f66bc45411f5f10bab
Elias <elias96.kodehode@gmail.com>
2026-09-14 21:08:23 +0200

    init

---
+++ backend/Hoyvik.API/wwwroot/assets/useNavigate-Cq93czn2.js
@@ -0,0 +1,1 @@
+import{o as e,r as t}from"./compiler-runtime-BAC6wZ-W.js";import{t as n}from"./useRouter-Br6bG42m.js";var r=e(t(),1);function i(e){let t=n();return r.useCallback(n=>t.navigate({...n,from:n.from??e?.from}),[e?.from,t])}export{i as t};
