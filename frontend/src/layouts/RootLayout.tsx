614209031fb3a552344adb2dba994b664a467bde
aesthaltics <aesthaltics@pm.me>
2026-09-19 18:45:38 +0200

    more unused stuff

--- frontend/src/layouts/RootLayout.tsx
+++ frontend/src/layouts/RootLayout.tsx
@@ -1,18 +1,7 @@
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
-import { Button } from "@/components/ui/button";
-import { Menu } from "lucide-react";
-import {
-    DropdownMenu,
-    DropdownMenuContent,
-    DropdownMenuGroup,
-    DropdownMenuItem,
-    DropdownMenuLabel,
-    DropdownMenuSeparator,
-    DropdownMenuTrigger
-} from "@/components/ui/dropdown-menu";
-import { Link, Outlet, useNavigate } from "@tanstack/react-router";
-import { useTheme } from "@/components/theme-provider";
-import { useAuth } from "@/hooks/authContext";
+
+import { Outlet } from "@tanstack/react-router";
+
 export default function RootLayout() {
     return (
         <>
