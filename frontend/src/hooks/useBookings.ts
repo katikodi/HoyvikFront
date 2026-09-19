3734033f631d76f2b080ef280934e5b49d607fea
aesthaltics <aesthaltics@pm.me>
2026-09-19 21:13:27 +0200

    removed unused import

--- frontend/src/hooks/useBookings.ts
+++ frontend/src/hooks/useBookings.ts
@@ -6,13 +6,6 @@ import {
     deleteBlockedBookigns
 } from "@/queries/booking.queries";
 
-function formatDateOnly(date: Date) {
-    const year = date.getFullYear();
-    const month = String(date.getMonth() + 1).padStart(2, "0");
-    const day = String(date.getDate()).padStart(2, "0");
-
-    return `${year}-${month}-${day}`;
-}
 const useBookings = () => {
     const queryClient = useQueryClient();
     let { data: bookedDates } = useQuery(occupiedBookingsQuery);
