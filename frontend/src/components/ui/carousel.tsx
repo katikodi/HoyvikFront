a82d4b605b7fcd58672ff5a3d7119501f1037b0d
aesthaltics <aesthaltics@pm.me>
2026-09-22 10:05:50 +0200

    moved carousel hooks

--- frontend/src/components/ui/carousel.tsx
+++ frontend/src/components/ui/carousel.tsx
@@ -1,43 +1,12 @@
 import * as React from "react";
 import { cn } from "cn";
-import useEmblaCarousel, { type UseEmblaCarouselType } from "embla-carousel-react";
 
 import { Button } from "@/components/ui/button";
 import { ChevronLeftIcon, ChevronRightIcon } from "lucide-react";
 
+import { useCarousel, type CarouselProps, CarouselContext } from "@/hooks/useCarousel";
+import useEmblaCarousel, { type UseEmblaCarouselType } from "embla-carousel-react";
 type CarouselApi = UseEmblaCarouselType[1];
-type UseCarouselParameters = Parameters<typeof useEmblaCarousel>;
-type CarouselOptions = UseCarouselParameters[0];
-type CarouselPlugin = UseCarouselParameters[1];
-
-type CarouselProps = {
-    opts?: CarouselOptions;
-    plugins?: CarouselPlugin;
-    orientation?: "horizontal" | "vertical";
-    setApi?: (api: CarouselApi) => void;
-};
-
-type CarouselContextProps = {
-    carouselRef: ReturnType<typeof useEmblaCarousel>[0];
-    api: ReturnType<typeof useEmblaCarousel>[1];
-    scrollPrev: () => void;
-    scrollNext: () => void;
-    canScrollPrev: boolean;
-    canScrollNext: boolean;
-} & CarouselProps;
-
-const CarouselContext = React.createContext<CarouselContextProps | null>(null);
-
-function useCarousel() {
-    const context = React.useContext(CarouselContext);
-
-    if (!context) {
-        throw new Error("useCarousel must be used within a <Carousel />");
-    }
-
-    return context;
-}
-
 function Carousel({
     orientation = "horizontal",
     opts,
